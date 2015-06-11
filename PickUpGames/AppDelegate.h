21f4cf7e13d4e760629baa10d4f174bf9301c469
Kaleo <kauakim@gmail.com>
2015-06-11 14:24:28 -0600

    inital commit

---
+++ PickUpGames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickUpGames
+//
+//  Created by Kaleo Kim on 6/11/15.
+//  Copyright (c) 2015 Kaleo Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PickUpGames/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PickUpGames
+//
+//  Created by Kaleo Kim on 6/11/15.
+//  Copyright (c) 2015 Kaleo Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
